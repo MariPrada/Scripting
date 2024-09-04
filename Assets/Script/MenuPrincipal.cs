5fac6a9f6f8468740a11fe6813d32b0ffde13866
Kenny <82070420+KennyM14@users.noreply.github.com>
2024-09-04 01:19:09 -0500

    Nivel 2

---
+++ Assets/Script/MenuPrincipal.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement; 
+
+public class MenuPrincipal : MonoBehaviour
+{
+    public void Play()
+    {
+        SceneManager.LoadScene("Nivel1"); 
+    }
+
+    public void Salir()
+    {
+        Debug.Log("Saliendo..."); 
+        Application.Quit();
+    }
+
+    public void Creditos()
+    {
+
+    }
+
+    public void Instrucciones()
+    {
+
+    }
+
+}
