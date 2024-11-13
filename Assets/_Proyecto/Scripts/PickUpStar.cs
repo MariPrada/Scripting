5349a7a07230e8fe5c3f407bd69eea465b2d01a7
José David Serna <marieladeleon123456789@gmail.com>
2024-11-13 14:27:18 -0500

    arreglando el boss

--- Assets/_Proyecto/Scripts/PickUpStar.cs
+++ Assets/_Proyecto/Scripts/PickUpStar.cs
@@ -1,9 +1,23 @@
 using MoreMountains.CorgiEngine;
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PickUpStar : RetroStar
 {
-  
+    [SerializeField] private int scoreGiven;
+    public TextMeshPro scoreBoard;
+    //Hacer un scoreboard que reciba y se actualice correctamente
+    public override void OnTriggerEnter2D(Collider2D collision)
+    {
+        _pickingCollider = collision;
+        _collidingObject = collision.gameObject;
+        PickItem(collision.gameObject);
+        Debug.Log($"Agarré {collision} de manera especial");
+        //La idea es que ahora podría usar las estrellas para meter un sistema de puntuación paralela a la recolección de estrellas o algo del estilo
+        //Pero no tengo tanto tiempo :c solo dejo la posibilidad ahi
+        scoreBoard.text += scoreGiven;
+    }
 }
